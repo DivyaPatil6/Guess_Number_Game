20dc0644fccc9712f826c35f2dbae7438d675ddf
DivyaPatil6 <80393665+DivyaPatil6@users.noreply.github.com>
2021-10-12 18:56:34 +0530

    Update and rename README.md to GuessNumber.c

---
+++ GuessNumber.c
@@ -0,0 +1,22 @@
+# Guess_Number_Game
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+   int number,guess,count=1;
+   srand(time(0));
+   number = rand()%100+1;
+   do{
+      printf("Guess the number between 1 to 100\n");
+      scanf("%d",&guess);
+      if(guess > number){
+	 printf("Enter Lower number...\n");
+      }else if(guess < number){
+	 printf("Enter Higher number...\n");
+      }else{
+	 printf("Congratulations you guess it in %d attempts\n",&count);
+      }
+      count++;
+   }while(guess != number);
+   return 0;
+}
